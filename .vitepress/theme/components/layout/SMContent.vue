b192dbf2fd42d854e61721615628f7d3b2b7067f
t4k <n.bakulina-developer@ya.ru>
2025-05-22 18:21:39 +0300

    feat: simple page

--- .vitepress/theme/components/layout/SMContent.vue
+++ .vitepress/theme/components/layout/SMContent.vue
@@ -2,6 +2,7 @@
 import { useData } from '../../composables/data'
 import SMHome from '../pages/SMHome.vue'
 import NotFound from '../pages/NotFound.vue'
+import SMSimplePage from '../pages/SMSimplePage.vue'
 
 const { page, frontmatter } = useData()
 </script>
@@ -10,5 +11,6 @@ const { page, frontmatter } = useData()
     <main>
         <NotFound v-if="page.isNotFound" />
         <SMHome v-else-if="frontmatter.layout === 'home'" />
+        <SMSimplePage v-else />
     </main>
 </template>
---
+++ .vitepress/theme/components/pages/SMSimplePage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useData } from '../../composables/data'
+const { frontmatter } = useData()
+</script>
+
+<template>
+    <div class="page__content">
+        <Content
+        :class="frontmatter.class" />
+    </div>
+</template>
