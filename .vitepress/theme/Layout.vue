5a4d9c805c6b9e6b7b89ecef601cc9c7d5eb14a9
t4k <n.bakulina-developer@ya.ru>
2025-05-22 17:36:30 +0300

    feat: cookies usage notification

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -2,10 +2,12 @@
 import SMNavBar from './components/layout/SMNavBar.vue'
 import SMContent from './components/layout/SMContent.vue'
 import SMFooter from './components/layout/SMFooter.vue'
+import SMCookies from './components/layout/SMCookies.vue'
 </script>
 
 <template>
   <SMNavBar />
   <SMContent />
   <SMFooter />
+  <SMCookies />
 </template>
---
+++ .vitepress/theme/components/layout/SMCookies.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const isHide = computed(() => {
+    if (typeof window !== 'undefined') {
+        return localStorage.getItem('cookies') === 'hide'
+    }
+    return false
+})
+
+const hide = () => {
+    if (typeof window !== 'undefined') {
+        localStorage.setItem('cookies', 'hide')
+    }
+    return false
+}
+</script>
+
+<template>
+    <div class="toast fade show cookies" v-if="!isHide">
+        <div class="toast-body">
+            Наш сайт использует файлы cookies.
+            Оставаясь на сайте, вы разрешаете обработку этих файлов.
+            Вы можете установить настройки cookies в вашем браузере.
+        </div>
+        <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="toast"
+            aria-label="Close"
+            @click="hide">
+        </button>
+    </div>
+</template>
