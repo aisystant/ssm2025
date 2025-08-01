24465b037f36c5213fc0c91c79ca51f999c7a62e
t4k <n.bakulina-developer@ya.ru>
2025-08-01 15:34:55 +0300

    feat: 404 page

--- .vitepress/theme/components/pages/NotFound.vue
+++ .vitepress/theme/components/pages/NotFound.vue
@@ -1,7 +1,31 @@
 <script setup lang="ts">
+import yamlText from '../../../../components/error.yml?raw'
+import yaml from 'js-yaml'
 
+const data = yaml.load(yamlText) as {
+    title: string
+    text: string
+    image: string
+    button?: string
+    link?: string
+}
 </script>
 
 <template>
-    404
+   <div class="page__error">
+        <div class="text-center">
+            <p class="h3" v-html="data.title"></p>
+
+            <img :src="`/images/${data.image}`" class="my-4" alt="Ошибка 404">
+
+            <p class="mb-4" v-html="data.text"></p>
+
+            <a
+                :href="data.link?.trim() ?? `/`"
+                class="btn btn-dark"
+                v-if="data.button">
+                {{ data.button }}
+            </a>
+        </div>
+    </div>
 </template>
