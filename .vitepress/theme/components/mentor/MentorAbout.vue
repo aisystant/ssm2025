11dd287e39232b60a5430930062062878aee0f01
t4k <n.bakulina-developer@ya.ru>
2025-08-07 15:24:26 +0300

    feat: add mentor about text

---
+++ .vitepress/theme/components/mentor/MentorAbout.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+    title: string,
+    text: string[],
+}>()
+</script>
+
+<template>
+    <section class="mentor-about">
+        <div class="container">
+            <div class="section-body">
+                <h2 class="about-title" v-html="title"></h2>
+
+                <div class="about-text">
+                    <template v-for="item in text" :key="item">
+                        <p v-html="item"></p>
+                    </template>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
