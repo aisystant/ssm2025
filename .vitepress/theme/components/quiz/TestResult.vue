84aa739e91cf6d9c99205fd9b22fee46b9bc2d7f
t4k <n.bakulina-developer@ya.ru>
2025-10-09 15:20:41 +0300

    chore: upgrade quiz

--- .vitepress/theme/components/quiz/TestResult.vue
+++ .vitepress/theme/components/quiz/TestResult.vue
@@ -48,6 +48,8 @@ onMounted(() => {
     </div>
 
     <div class="quiz-result" v-if="result">
+        <div class="result-subtitle">Ваш результат:</div>
+
         <div class="result-header" v-html="result.name"></div>
 
         <div class="result-body">
